95ce9128594cc1cc1518563e9dab251b7fb82f8e
Henrique Cezar <hcgv1@hotmail.com>
2026-03-22 18:41:36 -0300

    add permission to connect to the H2 database

--- noWaste/src/main/java/A3/project/noWaste/config/SecurityConfig.java
+++ noWaste/src/main/java/A3/project/noWaste/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // 🔥 THIS LINE
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
