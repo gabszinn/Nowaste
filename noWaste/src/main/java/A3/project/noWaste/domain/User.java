f102ac442914d738fe6295905c57b67d133399f5
Henrique Cezar <hcgv1@hotmail.com>
2026-03-22 10:07:12 -0300

    add User class

---
+++ noWaste/src/main/java/A3/project/noWaste/domain/User.java
@@ -0,0 +1,29 @@
+package A3.project.noWaste.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @NotBlank(message = "Nome não pode ser nulo")
+    private String Username;
+
+    @Column(unique = true)
+    @Email(message = "Email digitado inválido")
+    private String email;
+
+    @NotBlank(message = "Senha não poder nula")
+    private String password;
+
+
+}
