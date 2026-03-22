ad2268d5f21e38357261b65fef6c9d01ac49f4f6
Henrique Cezar <hcgv1@hotmail.com>
2026-03-22 11:01:23 -0300

    create user implementation

---
+++ noWaste/src/main/java/A3/project/noWaste/service/impl/UserImpl.java
@@ -0,0 +1,47 @@
+package A3.project.noWaste.service.impl;
+
+import A3.project.noWaste.domain.User;
+import A3.project.noWaste.infra.UserRepository;
+import A3.project.noWaste.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    // find a user
+    @Override
+    public User findById(Integer Id) {
+        Optional<User> obj = repository.findById(Id);
+        return obj.orElse(null);
+    }
+
+    // find all Users
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    // create User
+    @Override
+    public User create(User user) {
+        return repository.save(user);
+    }
+
+    // delete User
+    @Override
+    public boolean delete(Integer id) {
+        if (!repository.existsById(id)) {
+            return false;
+        }
+        repository.deleteById(id);
+        return true;
+    }
+
+}
