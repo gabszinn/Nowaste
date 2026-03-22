33f3098ec33f616d8421f8b4c702ed72377a173d
Henrique Cezar <hcgv1@hotmail.com>
2026-03-22 09:20:37 -0300

    Add Spring Boot project

---
+++ noWaste/src/main/java/A3/project/noWaste/NoWasteApplication.java
@@ -0,0 +1,13 @@
+package A3.project.noWaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoWasteApplication.class, args);
+	}
+
+}
---
+++ noWaste/src/test/java/A3/project/noWaste/NoWasteApplicationTests.java
@@ -0,0 +1,13 @@
+package A3.project.noWaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoWasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
