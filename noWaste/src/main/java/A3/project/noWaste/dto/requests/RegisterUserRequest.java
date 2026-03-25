75cdb5b969e7dfb9d230aaf0637f81515ece3ca5
Henrique Cezar <hcgv1@hotmail.com>
2026-03-25 15:51:10 -0300

    removing RegisterUserResponse and Request

--- noWaste/src/main/java/A3/project/noWaste/dto/requests/RegisterUserRequest.java
+++
@@ -1,8 +0,0 @@
-package A3.project.noWaste.dto.requests;
-
-import jakarta.validation.constraints.NotEmpty;
-
-public record RegisterUserRequest(@NotEmpty(message = "Username é obrigatório") String username,
-                                  @NotEmpty(message = "Email é obrigatório") String email,
-                                  @NotEmpty(message = "Senha é obrigatória") String password) {
-}
--- noWaste/src/main/java/A3/project/noWaste/dto/responses/RegisterUserResponse.java
+++
@@ -1,4 +0,0 @@
-package A3.project.noWaste.dto.responses;
-
-public record RegisterUserResponse(String username, String email) {
-}
