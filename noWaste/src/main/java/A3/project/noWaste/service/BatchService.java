ababf744e0ba0b23e2564ebfd055e08c2c125230
Henrique Cezar <hcgv1@hotmail.com>
2026-04-02 11:44:38 -0300

    Adding the methods to the batch service

--- noWaste/src/main/java/A3/project/noWaste/service/BatchService.java
+++ noWaste/src/main/java/A3/project/noWaste/service/BatchService.java
@@ -1,7 +1,20 @@
 package A3.project.noWaste.service;
 
 
-public class BatchService {
+import A3.project.noWaste.domain.Batch;
+import A3.project.noWaste.dto.BatchDTO;
 
+import java.util.List;
 
+public interface BatchService {
+
+    Batch findById(Integer inventoryId, Integer productId,Integer batchId);
+
+    List<Batch> findAllByProduct(Integer inventoryId, Integer productId);
+
+    Batch create(Integer inventoryId, Integer productId, BatchDTO obj);
+
+    Batch update(Integer inventoryId, Integer productId, Integer batchId, BatchDTO obj);
+
+    void delete(Integer inventoryId, Integer productId, Integer batchId);
 }
