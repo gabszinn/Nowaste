ea26f2d5d15857830e8b90313eeca715ad871835
Henrique Cezar <hcgv1@hotmail.com>
2026-04-02 11:44:11 -0300

    Implementing the batch repository

--- noWaste/src/main/java/A3/project/noWaste/infra/BatchRepository.java
+++ noWaste/src/main/java/A3/project/noWaste/infra/BatchRepository.java
@@ -2,10 +2,15 @@ package A3.project.noWaste.infra;
 
 import A3.project.noWaste.domain.Batch;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
+
 
 public interface BatchRepository extends JpaRepository<Batch, Integer> {
 
-    Optional<Batch> findBatchById(Integer id);
+    List<Batch> findByProductId(Integer productId);
+
+    Optional<Batch> findByIdAndProductId(Integer batchId, Integer productId);
+
+    boolean existsByCodeIgnoreCaseAndProductId(String code, Integer productId);
 }
