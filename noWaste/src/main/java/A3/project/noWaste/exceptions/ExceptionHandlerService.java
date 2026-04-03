505010355179ebc5026941b997ee9f302f64dafb
Henrique Cezar <hcgv1@hotmail.com>
2026-04-03 14:04:23 -0300

    adding the validationError class to ExceptionHandlerService

--- noWaste/src/main/java/A3/project/noWaste/exceptions/ExceptionHandlerService.java
+++ noWaste/src/main/java/A3/project/noWaste/exceptions/ExceptionHandlerService.java
@@ -3,6 +3,7 @@ package A3.project.noWaste.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,6 +12,21 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class ExceptionHandlerService {
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> methodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        ValidationError error = new ValidationError(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de validação",
+                request.getRequestURI()
+        );
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(fieldError -> error.addError(fieldError.getField() + ": " + fieldError.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<StandardError>userNotFoundException(UserNotFoundException ex, HttpServletRequest request) {
